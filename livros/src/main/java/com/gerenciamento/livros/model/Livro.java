303054d07dc761b043f847c54849a50d3cfba37b
Thiago Chapecó <guampas@uol.com.br>
2026-03-18 16:39:55 -0300

    crud-eliel-spring

---
+++ livros/src/main/java/com/gerenciamento/livros/model/Livro.java
@@ -0,0 +1,73 @@
+package com.gerenciamento.livros.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro {
+
+    private Long id;
+    @Column(name = "titulo_livro")
+    private String titulo;
+    @Column(name = "autor_livro")
+    private String autor;
+    private Integer anoPublicacao;
+
+    public Livro() {
+    }
+
+    public Livro(Long id, String titulo, String autor, Integer anoPublicacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(id, livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
